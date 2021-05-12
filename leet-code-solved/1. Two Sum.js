60f3a8fa885038cabd1bbbdfd66ecf7906f680ef
Restyled.io <commits@restyled.io>
2021-05-12 01:02:20 +0000

    Restyled by clang-format

--- leet-code-solved/1. Two Sum.js
+++ leet-code-solved/1. Two Sum.js
@@ -8,26 +8,27 @@
  * Time: O(n)
  * Space: O(n)
  */
-var twoSum = function (nums, target) {
+var twoSum = function(nums, target) {
   const hashtable = {};
 
   nums.forEach((num, index) => {
     if (hashtable[num]) {
       hashtable[num].push(index);
     } else {
-      hashtable[num] = [index];
+      hashtable[num] = [ index ];
     }
   });
 
   for (let i = 0; i < nums.length; i++) {
     const num = nums[i];
     if (hashtable[target - num]) {
-      if (target - num !== num) return [hashtable[num][0], hashtable[target - num][0]];
+      if (target - num !== num)
+        return [ hashtable[num][0], hashtable[target - num][0] ];
 
-      if (hashtable[num].length > 1) return [hashtable[num][0], hashtable[num][1]];
+      if (hashtable[num].length > 1)
+        return [ hashtable[num][0], hashtable[num][1] ];
     }
   }
 
   return [];
 };
-  
