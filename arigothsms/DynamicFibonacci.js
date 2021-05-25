475e81a2de939a268b1a5460b2526a45e1542668
Restyled.io <commits@restyled.io>
2021-05-25 07:06:19 +0000

    Restyled by prettier

--- arigothsms/DynamicFibonacci.js
+++ arigothsms/DynamicFibonacci.js
@@ -6,11 +6,9 @@ const dynamicFib = () => {
   const cache = {};
 
   return function fib(n) {
-    if (n in cache)
-      return cache[n];
+    if (n in cache) return cache[n];
 
-    if (n < 2)
-      return 1;
+    if (n < 2) return 1;
 
     return fib(n - 1) + fib(n - 2);
   };
