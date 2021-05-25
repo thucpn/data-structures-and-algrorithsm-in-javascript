06b20d48e9f05be740ab991b28ffb1c0f70b0fe2
Restyled.io <commits@restyled.io>
2021-05-25 07:06:19 +0000

    Restyled by clang-format

--- arigothsms/DynamicFibonacci.js
+++ arigothsms/DynamicFibonacci.js
@@ -6,12 +6,14 @@ const dynamicFib = () => {
   const cache = {};
 
   return function fib(n) {
-    if (n in cache) return cache[n];
+    if (n in cache)
+      return cache[n];
 
-    if (n < 2) return 1;
+    if (n < 2)
+      return 1;
 
     return fib(n - 1) + fib(n - 2);
   };
 };
- 
-console.log(dynamicFib()(3)); 
+
+console.log(dynamicFib()(3));
